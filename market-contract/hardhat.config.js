3ac420d64a2d55913d8de2ba49fd7788bee4eb73
annoydey <annoydey@gmail.com>
2023-12-04 03:33:54 -0500

    Fixed dot env file

--- market-contract/hardhat.config.js
+++ market-contract/hardhat.config.js
@@ -1,8 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const SEPOLIA_URL_KEY = "https://sepolia.infura.io/v3/a2db97898802468fb8f695398ec23b68";
+require("dotenv").config();
 
-const SEPOLIA_PRIVATE_KEY = "91163c3c2ca9eba99c797ab3ed621d457f77f4867cf4ee4c08f47986b82b95b7";
+const SEPOLIA_URL_KEY = process.env.SEPOLIA_URL_KEY;
+
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -14,3 +16,4 @@ module.exports = {
     }
   }
 };
+
